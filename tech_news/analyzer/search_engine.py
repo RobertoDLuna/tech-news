1c450a02e4608fd1e7a4be6c90781598823c8b46
Roberto Luna <robertocgw@gmail.com>
2023-02-09 14:08:30 -0300

    feat: req8 - Crie a função search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+import datetime
 
 # Requisito 7
 
@@ -13,7 +14,13 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        date = datetime.date.fromisoformat(date).strftime("%d/%m/%Y")
+        news = search_news({"timestamp": date})
+        list_news = [(new["title"], new["url"]) for new in news]
+    except ValueError:
+        raise ValueError("Data inválida")
+    return list_news
 
 
 # Requisito 9
